20da1760018ad6aa5bc5a64a7c378211ed60e3f6
Vadim <vadim.kurn@gmail.com>
2020-10-20 21:48:53 +0300

    Tests edited

--- src/test/java/ru/netology/manager/OfferManagerTest.java
+++ src/test/java/ru/netology/manager/OfferManagerTest.java
@@ -15,6 +15,7 @@ class OfferManagerTest {
     private FlightOffer thirdOffer = new FlightOffer(3, 5100, "ABA", "DME", 180);
     private FlightOffer fourthOffer = new FlightOffer(4, 3000, "VOG", "KRR",200);
     private FlightOffer fifthOffer = new FlightOffer(5, 6500, "ABA", "DME", 180);
+    private FlightOffer sixthOffer = new FlightOffer(6, 5000, "VTB", "DME", 150);
 
     @BeforeEach
     public void setUp() {
@@ -23,12 +24,13 @@ class OfferManagerTest {
         manager.add(thirdOffer);
         manager.add(fourthOffer);
         manager.add(fifthOffer);
+        manager.add(sixthOffer);
     }
 
     @Test
     void shouldFindSingle() {
-        FlightOffer[] expected = new FlightOffer[] {secondOffer};
-        FlightOffer[] actual = manager.findAll("VTB", "DME");
+        FlightOffer[] expected = new FlightOffer[] {fourthOffer};
+        FlightOffer[] actual = manager.findAll("VOG", "KRR");
         assertArrayEquals(expected, actual);
     }
 
@@ -40,9 +42,24 @@ class OfferManagerTest {
     }
 
     @Test
+    void shouldFindIfIdentical() {
+        FlightOffer[] expected = new FlightOffer[] {secondOffer, sixthOffer};
+        FlightOffer[] actual = manager.findAll("VTB", "DME");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     void shouldNotFindIfNotExist() {
         FlightOffer[] expected = new FlightOffer[] {};
         FlightOffer[] actual = manager.findAll("DME", "VTB");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotFindIfRepositoryIsEmpty() {
+        repository.removeAll();
+        FlightOffer[] expected = new FlightOffer[] {};
+        FlightOffer[] actual = manager.findAll("VOG", "KRR");
         assertArrayEquals(expected, actual);
     }
 }
