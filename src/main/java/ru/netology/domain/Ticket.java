168ebe2733cf7a4445767692d7310bba81c6a8c8
Vadim <vadim.kurn@gmail.com>
2020-10-20 12:32:08 +0300

    Classes added

---
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -0,0 +1,10 @@
+package ru.netology.ticket;
+
+public class Ticket implements Comparable {
+    private int id;
+    private int price;
+    private String srcAirport;
+    private String dstAirport;
+    private int flightTime;
+
+}
