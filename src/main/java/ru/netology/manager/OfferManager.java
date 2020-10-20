661bb725e002f671e43221f7ed0c0042fdda9100
Vadim <vadim.kurn@gmail.com>
2020-10-20 13:10:48 +0300

    OfferManager.java added

--- src/main/java/ru/netology/manager/OfferManager.java
+++ src/main/java/ru/netology/manager/OfferManager.java
@@ -1,4 +1,30 @@
 package ru.netology.manager;
 
+import ru.netology.domain.FlightOffer;
+import ru.netology.repository.OfferRepository;
+
 public class OfferManager {
+    private OfferRepository repository;
+
+    public OfferManager(OfferRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(FlightOffer item) {
+        repository.save(item);
+    }
+
+    public FlightOffer[] searchBy(String text) { //TODO Метод должен принимать на вход два параметра
+        FlightOffer[] result = new FlightOffer[0];
+        for (FlightOffer offer : repository.findAll()) {
+            if (offer.matches(text)) {
+                FlightOffer[] tmp = new FlightOffer[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                int lastIndex = tmp.length - 1;
+                tmp[lastIndex] = offer;
+                result = tmp;
+            }
+        }
+        return result;
+    }
 }
